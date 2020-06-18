861521e5453b70ef7e3217d2a61270aba1a6bd90
gitluin <kyluin@outlook.com>
2020-06-17 23:10:59 -0400

    new: runpace for getting min/mile

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This is a repository of mini projects that I think more people might find useful
 ## Excel CtoI
  
 This C program converts the provided spreadsheet column letter(s) (up to ZZ) into integers. This can be really useful if you work with poorly-formatted data like I do, or just data in general.
+
+## Runpace
+ 
+This C program calculates minutes/mile pace from [HH:MM:SS] and [Miles]. Might add support for other things later, but so far this is all I need.
---
+++ runpace/runpace.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int
+slen(const char* str){
+	int i = 0;
+
+	while (*str){ str++; i++; }
+
+	return i;
+}
+
+int
+main(int argc, char* argv[]) {
+	float hms[3];
+	float miles, pace, pace_min, pace_sec;
+	char* token;
+	char time[5], pace_str[5], mile_str[4];
+	char delim[2] = ":";
+	int i = 0;
+
+	if (argc != 3){
+		printf("Please provide 2 arguments: [HH:MM:SS] and [Miles]!\n");
+		return 0;
+	}
+
+	snprintf(time, 9 * sizeof(char), "%s", argv[1]);
+	snprintf(mile_str, 4 * sizeof(char), "%s", argv[2]);
+	miles = strtof(mile_str, NULL);
+
+	token = strtok(time, delim);
+	while (token){
+
+		if (i <= 3){
+			hms[i] = strtof(token, NULL);
+
+		} else {
+			break;
+		}
+
+		token = strtok(NULL, delim);
+		i++;
+	}
+
+	pace = ((hms[0] * 60) + hms[1] + (hms[2] / 60)) / miles;
+	pace_min = floor(pace);
+	pace_sec = (pace - pace_min) * 60;
+
+	snprintf(pace_str, 5 * sizeof(char),
+			(pace_sec < 10) ? "%.0f:0%.0f" : "%.0f:%.0f",
+			pace_min, pace_sec);
+
+	printf("%s\n", pace_str);
+
+	return 1;
+}
